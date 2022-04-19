cc8fa66963ca784d5b62d9a926e3b2a840e1e374
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-04-19 14:29:47 -0300

    sim

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Switch, Route } from 'react-router';
-import { BrowserRouter} from 'react-router-dom';
-
-import Login from './Login';
-import Register from './Register';
-import Dashboard from './Dashboard';
-import './Login.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Switch>
-            <Route exact path='/' component={Login} />
-            <Route exact path='/register' component={Register} />
-            <Route path='/dashboard' component={Dashboard} />
-           
-            {/* <Route component={NotFound}/> */}
-        </Switch>
-    </BrowserRouter>,
-    document.getElementById('root')
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
